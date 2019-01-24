c38edb9371bc09580d46fed54e7f058d882412ae
miguel myers <myers.m007@gmail.com>
2019-01-23 21:34:29 -0500

    additional functionality to tensor

--- framework/tensor.py
+++ framework/tensor.py
@@ -21,27 +21,69 @@ import numpy as np
 
 class Tensor (object):
 
-    def __init__(self, data, creators = None, creation_op = None):
+    def __init__(self, data, autograd = False, creators = None, creation_op = None, id = None):
+
         self.data = np.array(data)
         self.creation_op = creation_op
         self.creators = creators
         self.grad = None
+        self.autograd = autograd
+        self.children = {}
+        if(id is None):
+            id = np.random.randint(0,100000)
+        self.id = id
 
+        #   Keep track of how many children
+        if(creators is not None):
+            for c in creators:
+                if(self.id not in c.children):
+                    c.children[self.id] = 1
+                else:
+                    c.children[self.id] += 1
+
+    #  Check whether a Tensor has received the correct #
+    #  of gradients from each child.
+    def all_children_grads_accounted_for(self):
+        for id,cnt in self.children.items():
+            if(cnt != 0):
+                return False
+        return True
+
+    # begin actual backpropagation
     def __add__(self, other):
-        return  Tensor(self.data + other.data,
-                        creators = [self, other],
-                        creation_op = "add")
+        if(self.autograd and other.autograd):
+            return  Tensor(self.data + other.data,
+                            autograd = True,
+                            creators = [self, other],
+                            creation_op = "add")
+        return Tensor(self.data + other.data)
 
     def __repr__(self):
         return str(self.data.__repr__())
+
 
     def __str__(self):
         return str(self.data.__str__())
 
 
-    def backward(self, grad):
-        self.grad = grad
+    def backward(self, grad = None, grad_origin = None):
+        if(self.autograd):
+             # Check to make sure we can backpropagate or
+             # whether we are still waiting for a gradient,
+             # in which case decrement
+             # counter
+            if(grad_origin is not None):
+                if(self.children[grad_origin.id] == 0):
+                    raise Exception("cannot backpropgate more than once")
+                else:
+                    self.children[grad_origin.id] -= 1
+            if(self.grad is None):
+                # accumulate gradients from several children
+                self.grad = grad
+            else:
+                self.grad += grad
 
-        if(self.creation_op == "add"):
-            self.creators[0].backward(grad)
-            self.creators[1].backward(grad)
+            if(self.creators is not None and (self.all_children_grads_accounted_for() or grad_origin is None)):
+                if(self.creation_op == "add"):
+                    self.creators[0].backward(self.grad, self)
+                    self.creators[1].backward(self.grad, self)
--- framework/test-tensor.py
+++ framework/test-tensor.py
@@ -1,22 +1,25 @@
-from tensor import Tensor as tn
+from tensor import Tensor
 import unittest
+import numpy as np
 
-x = tn(5)
-
-y = x + x
-
-ls = [1,2,3,4]
-
-xx = Tensor([1,2,3,4,5])
-
-yy = Tensor([2,2,2,2,2])
 
 class MyTest(unittest.TestCase):
 
     def test(self):
+        x = Tensor(5)
+        y = x + x
         self.assertEqual(y.data, 10)
 
     def test1(self):
+        ls = [1,2,3,4]
         self.assertEqual(ls, [1,2,3,4])
 
-    
+    def test2(self):
+        a = Tensor([1,2,3,4,5], autograd=True)
+        b = Tensor([2,2,2,2,2], autograd=True)
+        c = Tensor([5,4,3,2,1], autograd=True)
+        d = a + b
+        e = b + c
+        f = d + e
+        f.backward(Tensor(np.array([1,1,1,1,1])))
+        self.assertEqual(b.grad.data.tolist(), np.array([2,2,2,2,2]).tolist())
